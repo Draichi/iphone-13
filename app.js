110d717d52e93c89b33fd4fc66938b327cccdba2
@Draichi <lucasdraichi@sabido.com>
2022-04-21 02:55:33 -0300

    feat: scrollTrigger animation

---
+++ app.js
@@ -0,0 +1,53 @@
+// Pin first page
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".first-page",
+        start: "0%",
+        end: "100%",
+        pin: true,
+        pinSpacing: false,
+    },
+});
+
+// Hightlight text
+const addTextHighlightTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: {
+            startColor: "blue",
+            endColor: "red",
+        },
+        scrub: true,
+        start: "-40%",
+        end: "40%",
+    },
+});
+
+const removeTextHighlightTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: {
+            startColor: "green",
+            endColor: "pink",
+        },
+        scrub: true,
+        start: "-20%",
+        end: "60%",
+    },
+});
+
+addTextHighlightTimeline.fromTo(
+    ".highlight",
+    {
+        color: "rgba(255,255,255,0.4)",
+    },
+    {
+        color: "rgba(255,255,255,1)",
+        stagger: 1,
+    }
+);
+
+removeTextHighlightTimeline.to(".highlight", {
+    color: "rgba(255,255,255,0.4)",
+    stagger: 1,
+});
