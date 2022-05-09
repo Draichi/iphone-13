1decfb0731ea44d220bbf5069d8e7040364a8ef5
@Draichi <lucasdraichi@sabido.com>
2022-05-08 23:13:04 -0300

    feat: carousel

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ gsap.timeline({
 const addTextHighlightTimeline = gsap.timeline({
     scrollTrigger: {
         trigger: ".second-page",
-        markers: {
-            startColor: "blue",
-            endColor: "red",
-        },
         scrub: true,
         start: "-40%",
         end: "40%",
@@ -26,10 +22,6 @@ const addTextHighlightTimeline = gsap.timeline({
 const removeTextHighlightTimeline = gsap.timeline({
     scrollTrigger: {
         trigger: ".second-page",
-        markers: {
-            startColor: "green",
-            endColor: "pink",
-        },
         scrub: true,
         start: "-20%",
         end: "60%",
@@ -59,10 +51,6 @@ const splitPhoneTimeline = gsap.timeline({
         start: "-25%",
         end: "30%",
         scrub: true,
-        markers: {
-            startColor: "red",
-            endColor: "black",
-        },
     },
 });
 
@@ -85,4 +73,68 @@ splitPhoneTimeline.fromTo(
     },
     '<'
 );
+splitPhoneTimeline.fromTo(
+    ".product-text-left",
+    {
+        x: 50,
+        opacity: 0
+    },
+    {
+        x: 0,
+        opacity: 1
+    },
+    '<'
+);
+splitPhoneTimeline.fromTo(
+    ".product-text-right",
+    {
+        x: -50,
+        opacity: 0
+    },
+    {
+        x: 0,
+        opacity: 1
+    },
+    '<'
+);
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".third-page",
+        start: "0",
+        end: "100%",
+        pin: true,
+        pinSpacing: false,
+    },
+});
 
+
+// Carousel
+const swatches = document.querySelectorAll('.swatches img')
+const gallery = document.querySelector('.phone-gallery')
+const slides = document.querySelectorAll('.phone-gallery-container')
+
+let currentSwatch = 'blue'
+let closeUpImageIndex = 2
+
+swatches.forEach((swatch, index) => {
+    const coordLeft = slides[index].getBoundingClientRect().left
+
+    swatch.addEventListener('click', e => {
+        const swatchName = e.target.getAttribute('swatch')
+        const closeUpImage = document.querySelector('.' + swatchName)
+
+        if (swatchName === currentSwatch) {
+            return
+        }
+
+        // Gallery
+        gsap.to(gallery, {x: -coordLeft, duration: 1, ease: 'back.out(1)'})
+
+        // CloseUp Image
+        gsap.set(closeUpImage, {zIndex: closeUpImageIndex})
+        gsap.fromTo(closeUpImage, {opacity: 0}, {opacity: 1, duration: 1})
+
+        closeUpImageIndex++
+        currentSwatch = swatchName
+    })
+})
